ba24ee072046f5e747cdaceb5896335208552518
Christopher Garcia <Christopherg1818@gmail.com>
2026-01-29 15:09:57 -0600

    new update

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,6 +4,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour{
     public float speed;
+    public GameObject ball;
+    private GameObject currentball;
+    public float offY  = -0.6f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
         
@@ -11,19 +14,36 @@ public class PlayerBehavior : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
-
-        if (true)
-        {
-            Vector3 = newPOs = transform.position;
+        if(currentball != null){
+            Vector3 playerPos = transform.position;
+            Vector3 ballOffset = new Vector3(0.0f, offY, 0.0f);
+            ball.transform.position = playerPos + ballOffset;
+        }
+        else{
+            currentball  = Instantiate(ball, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
         }
 
-        if (Keyboard.current.leftArrowKey.wasPressedThisFrame){
+        if(Keyboard.current.spaceKey.wasPressedThisFrame){
+            Rigidbody2D body = currentball.GetComponent<Rigidbody2D>();
+            body.gravityScale= 1.0f;
 
-            Vector3 newPos = transform.position;
-            newPos.x = newPos.x - speed;
-            transform.position = newPos;
-            print(newPos);
+            Collider2D collider  = currentball.GetComponent<Collider2D>();
+            collider.enabled = true;
 
+            currentball = null;
         }
+
+        //keyboard movement of player
+        float offset = 0.0f;
+        if(Keyboard.current.leftArrowKey.isPressed|| Keyboard.current.aKey.isPressed){
+            offset = -speed;
+        }
+
+        if(Keyboard.current.rightArrowKey.isPressed|| Keyboard.current.dKey.isPressed){
+            offset = speed;
+        }
+
+        Vector3 newPos = transform.position;
+        newPos.x = newPos.x + offset;
     }
 }
