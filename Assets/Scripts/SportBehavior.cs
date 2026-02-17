1091e5d6b346bbc65fe9ec63d8259c630c1c67d7
Christopher Garcia <Christopherg1818@gmail.com>
2026-02-17 15:13:42 -0600

    merging prefabs

--- Assets/Scripts/SportBehavior.cs
+++ Assets/Scripts/SportBehavior.cs
@@ -2,36 +2,65 @@ using UnityEngine;
 
 public class SportBehavior : MonoBehaviour
 {
-    // public float timeout = 3f;
-    // private float timeStart;
+    public float timeout;
+    public float timeStart;
+
+    public GameObject[] balls;
+    public int ballType;
 
 
-    // void Start(){ 
+    void Start(){ 
 
-    // } 
-    // void Update(){
+        balls = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehavior>().balls;
 
-    //  }
+    } 
+    void Update(){
 
-    // private void OnTriggerEnter2D(Collider2D other){
-    //     if (other.gameObject.CompareTag("Top")){
-    //         timeStart = Time.time;
-    //     }
-    // }
+     }
 
-    // private void OnTriggerStay2D(Collider2D other){
-    //     if (other.gameObject.CompareTag("Top")){
-    //         float timeThusfar = Time.time - timeStart;
-    //         if (timeThusfar > timeout){
+    public void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("fruit")){
+            int otherType = other.gameObject.GetComponent<SportBehavior>().ballType;
+            if(otherType == ballType &&  ballType < balls.Length-1){
+                //Destroy both thigs and create the merged one
+                if(gameObject.transform.position.x<= other.transform.position.x || 
+                (gameObject.transform.position.x== other.transform.position.x && gameObject.transform.position.y>= other.transform.position.y)){
+                    int choice = ballType+1;
+                    GameObject currentball  = Instantiate(balls[choice], Vector3.Lerp(gameObject.transform.position, other.gameObject.transform.position, 0.5f), Quaternion.identity);
+                    currentball.GetComponent<Collider2D>().enabled = true;
+                    currentball.GetComponent<Rigidbody2D>().gravityScale =1.0f;
+
+
+
+                    Destroy(other.gameObject);
+                    Destroy(gameObject);
+
+
+
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("Top")){
+            timeStart = Time.time;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other){
+        if (other.gameObject.CompareTag("Top")){
+            float timeThusfar = Time.time - timeStart;
+            if (timeThusfar > timeout){
                 
-    //             print("game over dude");
+                print("game over dude");
 
-    //         }
-    //     }
-    // }
-    // private void OnTriggerExit2D(Collider2D other){
-    //     if (other.gameObject.CompareTag("Top")){
-    //         timeStart = 0f;
-    //     }
-    // }
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other){
+        if (other.gameObject.CompareTag("Top")){
+            timeStart = 0f;
+        }
+    }
 }
